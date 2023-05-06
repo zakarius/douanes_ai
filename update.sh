7bd736ac29cbb1e3e2ffbaaade3e252ddec39b51
Bilali ZAKARI <zakarius228@gmail.com>
2023-05-06 10:14:14 +0000

    correct service

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 # Copy the files to the right place
 cp douanes-api.conf /etc/nginx/sites-available/
-cp douanes-api.service /etc/systemd/system/
+cp douanes-ai.service /etc/systemd/system/
 
 # Enable the service
 systemctl daemon-reload
-systemctl enable douanes-api.service
-systemctl start douanes-api.service
+systemctl enable douanes-ai.service
+systemctl start douanes-ai.service
 
 
 # Enable the site. if it's already enabled, disable it first
